8e6adb4d61f292705f1181d12a6dd1aecb0879e5
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2025-10-30 11:18:46 +0100

    _

--- Vérification_de_palindrome.js
+++ Vérification_de_palindrome.js
@@ -1,16 +1,11 @@
-
+const prompt = require("prompt-sync")();
 function isPalindrome(str) {
 
     
     const reversedStr = str.split('').reverse().join('');
     
-
-    
     return str === reversedStr;
 }
 
-
-console.log(`"level" -> ${isPalindrome("level")}`); 
-console.log(`"hello" -> ${isPalindrome("hello")}`); 
-console.log(`"radar" -> ${isPalindrome("radar")}`);
-console.log(`"test" -> ${isPalindrome("test")}`); 
+const V = prompt("entrez mot : ");
+console.log( `${V}-> ${isPalindrome(V)}`); 
